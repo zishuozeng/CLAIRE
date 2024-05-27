bed790b222beadafd51b0fe95ec8827a8dc07487
Billie-0918 <74541929+Billie-0918@users.noreply.github.com>
2024-05-27 11:15:07 +0800

    Add files via upload

---
+++ rxnfp_env.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# 创建 conda 环境
+conda create -n rxnfp python=3.6 -y
+
+# 激活 conda 环境
+source activate rxnfp
+
+# 安装必要的包
+conda install -c rdkit rdkit=2020.03.3 -y
+conda install -c tmap tmap -y
